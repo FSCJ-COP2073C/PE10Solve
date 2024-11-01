d4a47bdad6f8dd3740714dc1f93f3ef1b6bf7157
David Singletary <38020984+ProfSingletary@users.noreply.github.com>
2024-11-01 04:55:58 -0400

    Add files via upload

--- cop2073c-pe10Solve.R
+++ cop2073c-pe10Solve.R
@@ -1,7 +1,7 @@
 # cop2073c-pe10Solve.R
 # D. Singletary
 # 10/24/24
-# Use tidyverse and dplyr to manipulate flight data
+# Use the tidyverse and dplyr to manipulate flight data
 
 # install and load necessary packages
 # uncomment only if install required
@@ -11,8 +11,7 @@
 library(nycflights13)
 library(dplyr)
 
-# sort from longest to shortest delay
-delays <- arrange(flights, desc(dep_delay))
-
-# select times, carriers, and months
-select(delays, dep_delay, carrier, month)
+# Solution with pipes
+flights %>%
+  arrange(desc(distance), desc(air_time)) %>%
+  select(distance, air_time, carrier, month)
